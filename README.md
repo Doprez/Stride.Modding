28dedde15a7bacc642eedce9d5ac91012f4a2b4a
Doprez <73259914+Doprez@users.noreply.github.com>
2024-03-21 21:32:06 -0600

    added basic requirements

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # Stride.Modding
 Usefull functions for modding Stride
+
+# Future goals
+
+## Script loading
+[x] Loading DLLs at runtime
+[ ] Loading standalone scripts
+
+## Model importing
+[ ] GLTF loading (will likely use the code from [here](https://github.com/ykafia/StrideGltf/blob/master/GltfImport/LoadGLTF.cs) or just use the built in lib from Stride once its cross platform)
+[ ] FBX loading (I may just wait for the work to be done on the Stride importer https://github.com/stride3d/stride/pull/2163)
+
+## Misc
+At this point any other moddable features can be a seperate project since the base will be able to come from Stride.Modding.
+
+# Examples
+
+## Library Loading
+
+Create an interface as a contract for mods to follow
+
+``` csharp
+using Stride.Core;
+
+public interface ILoadable
+{
+	public string Name { get; }
+	public string Description { get; }
+
+	public void Initialize(IServiceRegistry services);
+}
+```
+
+Create a class to access the ModLoader
+``` csharp
+public class ModLoader : CustomModLoader<ILoadable>
+{
+	// You can override some methods here to implement your own loaders.
+}
+```
+
+finally you can create the instance how you want to enable loading mods at runtime. I usualy register everything in a Custom Game class and register it to the service registry from Stride.
+``` csharp
+public class CustomGame : Game
+{
+	private MyraRenderer _myraRenderer;
+	protected override void BeginRun()
+	{
+		base.BeginRun();
+		
+		ModLoader _modLoader = new();
+		Services.AddService(_modLoader);
+	}
+}
+```
+
+You can also register to events to be able to see when a mod is loaded and run a command. With the example above you can run `Initialize` whenever a mod is added.
+``` csharp
+private readonly ModLoader _modLoader
+
+// subscribe somewhere
+public Test(IServiceRegistry services)
+{
+	_modLoader = services.GetService<ModLoader>()
+	_modLoader.LoadedMods.CollectionChanged += InitializeMod;
+}
+
+private void InitializeMod(object? sender, TrackingCollectionChangedEventArgs e)
+{
+	if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+	{
+		var mod = (ILoadable)e.Item;
+		mod.Initialize(_services);
+	}
+}
+```
---
+++ src/Stride.Modding/Stride.Modding/BaseLibraryLoader.cs
@@ -0,0 +1,93 @@
+﻿using McMaster.NETCore.Plugins;
+using Stride.Core.Collections;
+
+namespace Stride.Modding;
+public abstract class BaseLibraryLoader<T>
+{
+	/// <summary>
+	/// Keeps track of all loaded mods.
+	/// </summary>
+	public TrackingHashSet<T> LoadedMods { get; private set; } = [];
+
+	/// <summary>
+	/// Tries to load any DLL files in the specified folder
+	/// </summary>
+	/// <param name="baseModFolder"></param>
+	public virtual void LoadMods(string baseModFolder)
+	{
+		var loaders = new List<PluginLoader>();
+
+		// create plugin loaders
+		foreach (var dir in Directory.GetDirectories(baseModFolder))
+		{
+			var dirName = Path.GetFileName(dir);
+			var pluginDll = Path.Combine(dir, dirName + ".dll");
+			if (File.Exists(pluginDll))
+			{
+				var loader = PluginLoader.CreateFromAssemblyFile(
+				pluginDll,
+				sharedTypes: [typeof(T)]);
+				loaders.Add(loader);
+			}
+		}
+
+		// Create an instance of plugin types
+		foreach (var loader in loaders)
+		{
+			try
+			{
+				foreach (var pluginType in loader
+					.LoadDefaultAssembly()
+					.GetTypes()
+					.Where(t => typeof(T).IsAssignableFrom(t) && !t.IsAbstract))
+				{
+					// This assumes the implementation of ILoadable has a parameterless constructor
+					T plugin = (T)Activator.CreateInstance(pluginType);
+					LoadedMods.Add(plugin);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Error loading plugin: {e.Message}");
+			}
+		}
+	}
+
+	/// <summary>
+	/// Loads a specific DLL file
+	/// </summary>
+	/// <param name="path"></param>
+	public virtual void LoadMod(string path)
+	{
+		var loaders = new List<PluginLoader>();
+
+		if (File.Exists(path))
+		{
+			var loader = PluginLoader.CreateFromAssemblyFile(
+			path,
+			sharedTypes: [typeof(T)]);
+			loaders.Add(loader);
+		}
+
+		// Create an instance of plugin types
+		foreach (var loader in loaders)
+		{
+			try
+			{
+				foreach (var pluginType in loader
+				.LoadDefaultAssembly()
+				.GetTypes()
+				.Where(t => typeof(T).IsAssignableFrom(t) && !t.IsAbstract))
+				{
+					// This assumes the implementation of ILoadable has a parameterless constructor
+					T plugin = (T)Activator.CreateInstance(pluginType);
+					LoadedMods.Add(plugin);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Error loading plugin: {e.Message}");
+			}
+		}
+	}
+}
